c29c6b839e8932707ee18323df28026d2a1a5399
penmanboman <rijinpenman@gmail.com>
2024-10-11 11:58:07 +0800

    Update _worker.js
    改变uuid

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
+let userID = '2e35c561-e2bc-4e31-9341-db8bfa43b5eb';
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
 const proxyIPs= ['bpb.yousef.isegaro.com'];
